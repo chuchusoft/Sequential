0555fe02a4af24865e38684a0144998b13b4ad31
chuchusoft <chuchusoft@gmail.com>
2023-11-13 20:39:32 +1100

    ARC conversion

--- Models/Adapter/PGHTMLAdapter.h
+++ Models/Adapter/PGHTMLAdapter.h
@@ -29,10 +29,12 @@ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 extern NSString *const PGDOMDocumentKey;
 
 @interface PGHTMLAdapter : PGContainerAdapter <WKNavigationDelegate>
+#if !__has_feature(objc_arc)
 {
 	@private
 	WKWebView *_webView;	//	WebView *_webView;
 	WKNavigation *_navigation;
 }
+#endif
 
 @end
