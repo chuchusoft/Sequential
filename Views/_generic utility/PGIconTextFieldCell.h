da30463ecfc7f245a74fd1fc34a00c1e3e323733
chuchusoft <chuchusoft@gmail.com>
2023-11-13 22:44:25 +1100

    ARC conversion

--- Views/_generic utility/PGIconTextFieldCell.h
+++ Views/_generic utility/PGIconTextFieldCell.h
@@ -23,11 +23,15 @@ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
 @interface PGIconTextFieldCell : NSTextFieldCell <NSCopying>
+#if !__has_feature(objc_arc)
 {
 	@private
 	NSImage *_icon;
 }
 
 @property(retain) NSImage *icon;
+#else
+@property (nonatomic, strong) NSImage *icon;
+#endif
 
 @end
