ad16223f14d639c0c52df5b9ee6fec81ffb0e62d
chuchusoft <chuchusoft@gmail.com>
2023-11-16 22:00:38 +1100

    first commit

---
+++ Controllers/FullSizeContentController/PGFullSizeContentTitlebarAccessoryViewController.m
@@ -0,0 +1,12 @@
+//
+//  PGFullSizeContentTitlebarAccessoryViewController.m
+//  Sequential
+//
+//  Created by VincentTanP on 16/11/2023.
+//
+
+#import "PGFullSizeContentTitlebarAccessoryViewController.h"
+
+@implementation PGFullSizeContentTitlebarAccessoryViewController
+
+@end
